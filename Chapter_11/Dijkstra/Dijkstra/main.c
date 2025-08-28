d1e69cba369580bcf437cf31c3fcbeec0a352c06
Lee Heegeon <hptlord@gmail.com>
2025-08-28 17:04:41 +0900

    Dijkstra 알고리즘 구현

---
+++ Chapter_11/Dijkstra/Dijkstra/main.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_VERTICIES 100
+#define INF 1000000
+
+typedef struct {
+	int n;
+	int weight[MAX_VERTICIES][MAX_VERTICIES];
+}Graph;
+
+int distance[MAX_VERTICIES];
+int selected[MAX_VERTICIES];
+
+void dijkstra(Graph g, int start_v);
+int get_minpos(int n);
+void print_status(int n);
+
+int main(void)
+{
+	Graph g = { 7,
+		{{0,7,INF,INF,3,10,INF},
+		{7,0,4,10,2,6,INF},
+		{INF,4,0,2,INF,INF,INF},
+		{INF,10,2,0,11,9,4},
+		{3,2,INF,11,0,INF,5},
+		{10,6,INF,9,INF,0,INF},
+		{INF,INF,INF,4,5,INF,0}}
+	};
+	dijkstra(g, 0);
+	return 0;
+
+}
+
+
+
+void dijkstra(Graph g, int start_v)
+{
+	int u;
+	for (int i = 0; i < g.n; i++)
+	{
+		distance[i] = g.weight[start_v][i];
+		selected[i] = 0;
+	}
+	selected[start_v] = 1;
+
+	for (int i = 0; i < g.n - 1; i++)
+	{
+		print_status(g.n);
+		u = get_minpos(g.n);
+		selected[u] = 1;
+		for (int j = 0; j < g.n; j++)
+			if (!selected[j] && distance[j] > distance[u] + g.weight[u][j])
+				distance[j] = distance[u] + g.weight[u][j];
+	}
+	print_status(g.n);
+}
+
+int get_minpos(int n)
+{
+	int minpos;
+	int min = INF;
+	for (int i = 0; i < n; i++)
+		if (!selected[i] && distance[i] < min) {
+			min = distance[i];
+			minpos = i;
+		}
+	return minpos;
+}
+
+void print_status(int n)
+{
+	static int step = 1;
+	printf("STEP %d\n", step++);
+	printf("distance : ");
+	for (int i = 0; i < n; i++)
+	{
+		if (distance[i] == INF) printf("%3c ", '*');
+		else printf("%3d ", distance[i]);
+	}
+	printf("\n");
+
+	printf("selected : ");
+	for (int i = 0; i < n; i++)
+		printf("%3d ", selected[i]);
+	printf("\n\n");
+}
