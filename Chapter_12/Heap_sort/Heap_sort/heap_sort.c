86aeb2fa48e0339b9b064ab4b1374e8250ec4847
Lee Heegeon <hptlord@gmail.com>
2025-09-25 18:39:59 +0900

    12강 완료

--- Chapter_12/Heap_sort/Heap_sort/heap_sort.c
+++ Chapter_12/Heap_sort/Heap_sort/heap_sort.c
@@ -9,63 +9,108 @@ void start()
 	int num = 0;
 	printf("숫자의 개수 : ");
 	scanf("%d", &num);
-	
-	int* heap = (int*)malloc((num + 1) * sizeof(int));
-	if (heap == NULL)
-	{
-		printf("NULL값 할당 오류\n");
-		exit(1);
-	}
+
+	Heap h;
+	init_heap(&h, num);
 
 	srand(time(NULL));
 	for (int i = 0; i < num; i++)
 	{
-		int n = rand() % 100;
-		insert_heap(heap, n);
+		int data = rand() % 100;
+		printf("%d ", data);
+		insert_heap(&h, data);
 	}
+	printf("\n\n");
 
-	print_heap(heap, num);
-	printf("\n");
-
-	heap_sort(heap, num);
+	heap_sort(&h, num);
 	printf("\n");
 
 	printf("정렬된 배열 : \n");
-	print_heap(heap, num);
+	print_list(&h, num);
 }
-
-void heap_sort(int list[], int n)
+void init_heap(Heap* h, int heap_size)
 {
-	for (int i = n; i > 0; i--)
+	h->size = 0;
+	h->list = (int*)malloc((heap_size + 1) * sizeof(int));
+	if (h->list == NULL)
 	{
-		list[i] = delete_heap(list, i);
-		print_heap_sort(list, i, n);
+		printf("NULL값 할당 오류\n");
+		exit(1);
 	}
 }
 
-void print_heap(int list[], int n)
+void heap_sort(Heap* h, int list_size)
 {
-	for (int i = 1; i <= n; i++)
-		printf("%d ", list[i]);
+	for (int i = list_size; i > 0; i--)
+	{
+		h->list[i] = delete_heap(h);
+		print_heap_sort(h, list_size);
+	}
+}
+
+void print_list(Heap* h, int list_size)
+{
+	for (int i = 1; i <= list_size; i++)
+		printf("%d ", h->list[i]);
 	printf("\n");
 }
 
-void print_heap_sort(int list[], int heap_size, int n)
+void print_heap_sort(Heap* h, int list_size)
 {
 	int i = 1;
-	for (i; i < heap_size; i++)	
-		printf("%d ", list[i]);
+	for (i; i <= h->size; i++)
+		printf("%d ", h->list[i]);
 	printf("[");
-	for (i; i < n; i++)
-		printf("%d ", list[i]);
-	printf("%d]\n", list[n]);
+	for (i; i < list_size; i++)
+		printf("%d ", h->list[i]);
+	printf("%d]\n", h->list[list_size]);
 }
 
-void insert_heap(int heap[], int n)
+void insert_heap(Heap* h, int data)
 {
-
+	h->size++;
+	int i = h->size;
+	while (i > 1)
+	{
+		if (data > h->list[i / 2])
+		{
+			h->list[i] = h->list[i / 2];
+			i /= 2;
+		}
+		else break;
+	}
+	h->list[i] = data;
 }
-int delete_heap(int heap[], int n)
-{
 
+int delete_heap(Heap* h)
+{
+	if (h->size < 1)
+	{
+		printf("히프가 공백상태입니다.\n");
+		return;
+	}
+
+	int max = h->list[1];
+	int last = h->list[h->size--];
+	int parent = 1;
+	int	child = 2 * parent;
+	int tmp;
+	while (child <= h->size)
+	{
+		if (child == h->size) 
+			tmp = child;
+		else 
+			tmp = h->list[child] > h->list[child + 1] ? child : child + 1;
+
+		if (last < h->list[tmp])
+		{
+			h->list[parent] = h->list[tmp];
+			parent = tmp;
+			child = 2 * parent;
+		}
+		else break;
+	}
+	h->list[parent] = last;
+
+	return max;
 }
--- Chapter_12/Heap_sort/Heap_sort/heap_sort.h
+++ Chapter_12/Heap_sort/Heap_sort/heap_sort.h
@@ -1,13 +1,20 @@
 #ifndef HEAP_SORT_H
 #define HEAP_SORT_H
 
-void start();
-void heap_sort(int list[], int n);
-void print_heap(int list[], int n);
-void print_heap_sort(int list[], int heap_size, int n);
+typedef struct
+{
+	int size;
+	int* list;
+}Heap;
 
-void insert_heap(int heap[], int n);
-int delete_heap(int heap[], int n);
+void start();
+void init_heap(Heap* h, int heap_size);
+void heap_sort(Heap* h, int list_size);
+void print_list(Heap* h, int list_size);
+void print_heap_sort(Heap* h, int n);
+
+void insert_heap(Heap* h, int data);
+int delete_heap(Heap* h);
 
 #endif // !HEAP_SORT_H
 
